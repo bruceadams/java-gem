dae42c41a557a6a45a92fff229cb7be591e68eef
Bruce Adams <bruce.adams@acm.org>
2012-05-26 15:04:40 -0400

    Bare bones Clojure project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject java-gem "1.0.0-SNAPSHOT"
+  :description "A tool for creating a Ruby Gem from a Java package and all of its dependencies."
+  :url "https://github.com/bruceadams/java-gem"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [leiningen-core "2.0.0-preview4"]])
---
+++ test/java_gem/core_test.clj
@@ -0,0 +1,7 @@
+(ns java-gem.core-test
+  (:use clojure.test
+        java-gem.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
